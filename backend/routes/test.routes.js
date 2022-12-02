1f0af5575a25a9cdccd95d10e58c436a3e5913c0
vladman25 <vladandreiman@gmail.com>
2022-12-03 00:39:04 +0200

    backend 4

--- backend/routes/test.routes.js
+++ backend/routes/test.routes.js
@@ -27,6 +27,10 @@ router.post("/",async (req,res) => {
         console.log('req.body')
         console.log(req.body)
 
+        let newDesc = req.body.description
+        if (newDesc.length > 2500) {
+            return res.status(400).send({message: "description too long"})
+        }
         const testElement = new Memes({
             description: req.body.description
         })
@@ -35,7 +39,7 @@ router.post("/",async (req,res) => {
         await testElement.save()
         console.log("After save:")
         console.log(testElement)
-        return res.send("OK")
+        return res.status(200).send("OK")
     } catch(error) {
         console.log(error)
         return res.status(500).send("Am murit")
@@ -47,7 +51,7 @@ router.patch("/:id",async (req,res) => {
         const data = await Memes.findByIdAndUpdate(req.params.id, {description: req.body.description});
         console.log('edited data')
         console.log(data)
-        return res.send("OK")
+        return res.status(200).send("OK")
     } catch(error) {
         console.log(error)
         return res.status(500).send("Am murit")
@@ -60,7 +64,7 @@ router.delete("/:id",async (req,res) => {
         const data = await Memes.findByIdAndDelete(req.params.id);
         console.log('deleted user')
         console.log(data)
-        return res.send("OK")
+        return res.status(200).send("OK")
     } catch(error) {
         console.log(error)
         return res.status(500).send("Am murit")
@@ -73,17 +77,63 @@ router.post("/register", async (req,res) => {
     try {
         console.log('req.body')
         console.log(req.body)
-        const testElement = new Users({
-            email: req.body.email,
-            username: req.body.username,
-            password: bcrypt.hashSync(req.body.password, salt)
-        })
-        console.log("Before save:")
-        console.log(testElement)
-        await testElement.save()
-        console.log("After save:")
-        console.log(testElement)
-        return res.send("OK")
+        let newEmail = req.body.email
+        let newUsername = req.body.username
+        let nuL = newUsername.length
+        let newPassword = req.body.password
+        let npL = newPassword.length
+
+        let response = new Object;
+        let ok = false;
+        if(newEmail == '') {
+            response.email = "is missing"
+            ok = true;
+        }
+        if(newUsername == '') {
+            response.username = "is missing"
+            ok = true;
+        }
+        if(newPassword == '') {
+            response.password = "is missing"
+            ok = true;
+        }
+
+        if(ok) {
+            return res.status(400).send(response)
+        }
+        var data = null
+        data = await Users.findOne({username: req.body.username})
+        if(data != null) {
+            return res.status(400).send({username:"already exists"})
+        }
+
+
+        if(newEmail.endsWith("@stud.acs.upb.ro") && ((nuL > 7)&&(nuL < 33))  && ((npL > 7)&&(npL < 33))) {
+            const testElement = new Users({
+                email: req.body.email,
+                username: req.body.username,
+                password: bcrypt.hashSync(req.body.password, salt)
+            })
+            console.log("Before save:")
+            console.log(testElement)
+            await testElement.save()
+            console.log("After save:")
+            console.log(testElement)
+            return res.status(200).send(testElement)
+        } else {
+            response = new Object;
+            if(!newEmail.endsWith("@stud.acs.upb.ro")) {
+                response.email = "the field must end in @stud.acs.upb.ro"
+            }
+            if(!((nuL > 7)&&(nuL < 33))) {
+                response.username = "the field must be between 8 and 32 charachetrs"
+            }
+            if(!((npL > 7)&&(npL < 33))) {
+                response.password = "the field must be between 8 and 32 charachetrs"
+            }
+            return res.status(400).send(response)
+        }
+
     } catch(error) {
         console.log(error)
         return res.status(500).send("Am murit")
@@ -94,6 +144,26 @@ router.post("/login", async (req,res) => {
     try {
         console.log('req.body')
         console.log(req.body)
+
+        let newUsername = req.body.username
+        let newPassword = req.body.password
+
+        let response = new Object;
+        let ok = false;
+        if(newUsername == '') {
+            response.username = "is missing"
+            ok = true;
+        }
+        if(newPassword == '') {
+            response.password = "is missing"
+            ok = true;
+        }
+
+        if(ok) {
+            return res.status(400).send(response)
+        }
+
+
         const data = await Users.findOne({username: req.body.username})
 
         if(data) {
@@ -104,15 +174,15 @@ router.post("/login", async (req,res) => {
                 var token = jwt.sign(req.body, SECRET);
 
                 // return res.send("OK")
-                return res.send({'jwt-token': token})
+                return res.status(200).send({'token': token})
             }
             console.log(myPlaintextPassword)
             console.log(hash)
-            return res.send("Wrong pass") 
+            return res.status(400).send({message: "Wrong email or password"})
                 
 
         } else {
-            return res.send("No user")
+            return res.status(400).send({message: "Wrong email or password"})
         }
 
     } catch(error) {
